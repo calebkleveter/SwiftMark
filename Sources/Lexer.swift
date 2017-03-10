86923f089db08e2ee149893cc1ffcfc6c90ec10a
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-10 07:40:05 -0600

    Sources/Lexer.swift - Created Lexer.Token.html CP

--- Sources/Lexer.swift
+++ Sources/Lexer.swift
@@ -42,5 +42,37 @@ class Lexer {
         case code(String)
         case escape(String)
         case text(String)
+        
+        var html: String {
+            switch self {
+            case .header1(let string): return "<h1>\(string)</h1>"
+            case .header2(let string): return "<h2>\(string)</h2>"
+            case .header3(let string): return "<h3>\(string)</h3>"
+            case .header4(let string): return "<h4>\(string)</h4>"
+            case .header5(let string): return "<h5>\(string)</h5>"
+            case .header6(let string): return "<h6>\(string)</h6>"
+            case .bold(let string): return "<strong>\(string)</strong>"
+            case .italic(let string): return "<em>\(string)</em>"
+            case .link(text: let string, url: let url): return "<a href=\"\(url)\">\(string)</a>"
+            case .image(text: let string, url: let url): return "<img src=\"\(url)\" alt=\"\(string)\">"
+            case .blockQuote(let strings):
+                let paragraphes = strings.map({ return "<p>\($0)</p>" }).joined()
+                return "<blockquote>\(paragraphes)</blockquote>"
+            case .orderedList(let strings):
+                let listItems = strings.map({ return "<li>\($0)</li>" }).joined()
+                return "<ol>\(listItems)</ol>"
+            case .unOrderedList(let strings):
+                let listItems = strings.map({ return "<li>\($0)</li>" }).joined()
+                return "<ul>\(listItems)</ul>"
+            case .codeBlock(let strings):
+                let lines = strings.map({ return "\($0)\n" }).joined()
+                return "<pre><code>\(lines)</code></pre>"
+            case .horizontalRule: return "<hr />"
+            case .code(let string): return "<code>\(string)</code>"
+            case .escape(let string): return string
+            case .text(let string): return string
+            }
+        }
+
     }
 }
