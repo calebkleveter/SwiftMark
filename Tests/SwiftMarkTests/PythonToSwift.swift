80e270192c7d6e6da210ef5b34852fdc53b9a6cf
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-03-29 15:31:53 -0500

    Tests/SwiftMarkTests/PythonToSwift.swift - Added runScript(user path) function

--- Tests/SwiftMarkTests/PythonToSwift.swift
+++ Tests/SwiftMarkTests/PythonToSwift.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+func runScript(fromUser user: String, withPath path: String) -> String? {
+    
+    let scriptPath: String = "/Users/\(user)/\(path)"
+    
+    let arguments = [scriptPath]
+    
+    let outPipe = Pipe()
+    let errPipe = Pipe();
+    
+    let task = Process()
+    task.launchPath = "/usr/bin/python"
+    task.arguments = arguments
+    task.standardInput = Pipe()
+    task.standardOutput = outPipe
+    task.standardError = errPipe
+    task.launch()
+    
+    let data = outPipe.fileHandleForReading.readDataToEndOfFile()
+    task.waitUntilExit()
+    
+    let exitCode = task.terminationStatus
+    if (exitCode != 0) {
+        print("ERROR: \(exitCode)")
+        return nil
+    }
+    
+    return String(data: data, encoding: String.Encoding.ascii)
+}
