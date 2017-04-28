6aa89e26766e987b74295c94ee2e5ac660cc0211
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-04-28 12:19:46 -0500

    Sources/Lexer.swift - Fixed unordered list nesting

--- Sources/Lexer.swift
+++ Sources/Lexer.swift
@@ -38,9 +38,9 @@ open class Lexer {
         ("\\!\\[(.+)\\]\\((.+)\\)",  ["$1", "$2"], { return .image(text: $0[0].safetyHTMLEncoded(), url: $0[1])}),
         ("\\[(.+)\\]\\((.+)\\)", ["$1", "$2"], { return .link(text: try Lexer().tokenize($0[0].safetyHTMLEncoded()), url: $0[1])}),
         ("\\>\\s?([^\\n\\>]+)", ["$1"], { return .blockQuote(try Lexer().tokenize($0[0]))}),
+        ("(((\\-|\\_|\\*)[\\s]?){3,})$", [], { _ in return .horizontalRule}),
         ("(\\+|\\-|\\*)\\s?(.+)", ["$2"], { return .unOrderedList(try Lexer().tokenize($0[0].safetyHTMLEncoded()))}),
         ("\\d+\\.\\s?([^\\n]+)", ["$1"], { return .orderedList(try Lexer().tokenize($0[0].safetyHTMLEncoded()))}),
-        ("((\\-|\\_|\\*)[\\s]?){3,}", [], { _ in return .horizontalRule}),
         ("\\`(.*)\\`", ["$1"], { return .code($0[0].safetyHTMLEncoded())}),
         ("\n{2}", [], { _ in return .break }),
         ("([^\\s]+)", ["$1"], { return .text($0[0].safetyHTMLEncoded())})
