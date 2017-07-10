94495f4ca136e0a44131fb93e67ae021add112f9
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-07-10 18:05:57 -0500

    Created Renderer.renderNode(node) method

--- Sources/Renderer/Renderer.swift
+++ Sources/Renderer/Renderer.swift
@@ -28,4 +28,47 @@ open class Renderer {
     public init(ast: AST) {
         self.ast = ast
     }
+    
+    private func renderText(_ node: ParagraphNode, withParagraph: Bool)throws -> String {
+        let text = try node.content.map(renderNode).joined()
+        if withParagraph {
+            return "<p>\(text)</p>"
+        } else {
+            return "\(text)"
+        }
+    }
+    
+    public func renderNode(_ node: ElementNode)throws -> String {
+        switch node {
+        case let node as TextNode: return node.value
+        case let node as HeaderOneNode: return "<h1>\(try node.content.map(renderNode).joined())</h1>"
+        case let node as HeaderTwoNode: return "<h2>\(try node.content.map(renderNode).joined())</h2>"
+        case let node as HeaderThreeNode: return "<h3>\(try node.content.map(renderNode).joined())</h3>"
+        case let node as HeaderFourNode: return "<h4>\(try node.content.map(renderNode).joined())</h4>"
+        case let node as HeaderFiveNode: return "<h5>\(try node.content.map(renderNode).joined())</h5>"
+        case let node as HeaderSixNode: return "<h6>\(try node.content.map(renderNode).joined())</h6>"
+        case let node as BoldNode: return "<strong>\(try node.content.map(renderNode).joined())</strong>"
+        case let node as ItalicNode: return "<em>\(try node.content.map(renderNode).joined())</em>"
+        case let node as LinkNode: return "<a href=\"\(node.url)\">\(try node.text.map(renderNode).joined())</a>"
+        case let node as ImageNode: return "<img src=\"\(node.url)\" alt=\"\(node.text)\"/>"
+        case _ as HorizontalRuleNode: return "<hr/>"
+        case let node as CodeNode: return "<code>\(node.value)</code>"
+        case let node as ParagraphNode: return "<p>\(try node.content.map(renderNode).joined())</p>"
+        case let node as CodeBlockNode: return "<pre><code>\(node.code)</code></pre>"
+        case let node as OrderedListNode:
+            let elements = try node.content.map({ element in
+                return "<li>\(try renderText(element as! ParagraphNode, withParagraph: false))</li>"
+            })
+            return "<ol>\(elements)</ol>"
+        case let node as UnorderedListNode:
+            let elements = try node.content.map({ element in
+                return "<li>\(try renderText(element as! ParagraphNode, withParagraph: false))</li>"
+            })
+            return "<ul>\(elements)</ul>"
+        case let node as BlockquoteNode:
+            let elements = try node.content.map({return try renderText($0 as! ParagraphNode, withParagraph: true)}).joined()
+            return "<blockquote>\(elements)</blockquote>"
+        default: fatalError("Unsupported Node")
+        }
+    }
 }
