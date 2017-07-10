b56333b1454e08de6e7d1e40eba6500cadf5cb48
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-07-10 06:28:45 -0500

    Organized project structure

--- Package.swift
+++ Package.swift
@@ -1,5 +1,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftMark"
+    name: "SwiftMark",
+    targets: [
+        Target(name: "Lexer"),
+        Target(name: "Parser", dependencies: ["Lexer"]),
+        Target(name: "Renderer", dependencies: ["Parser"]),
+        Target(name: "SwiftMark", dependencies: ["Lexer", "Parser", "Renderer"])
+    ]
 )
