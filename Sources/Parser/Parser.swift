c83f2c81550a752921747aace1658946f05390df
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-07-10 17:27:40 -0500

    Created AST type

--- Sources/Parser/Parser.swift
+++ Sources/Parser/Parser.swift
@@ -19,7 +19,10 @@
 //LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //SOFTWARE.
+
 import Lexer
+
+public typealias AST = [ElementNode]
 
 open class Parser {
     private let tokens: [Lexer.Token]
