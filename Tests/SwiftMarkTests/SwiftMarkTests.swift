4ba25f160f64e3738ad9855f8281a198f8c7e46e
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-07-11 08:52:53 -0500

    Fixed image markdown for test

--- Tests/SwiftMarkTests/SwiftMarkTests.swift
+++ Tests/SwiftMarkTests/SwiftMarkTests.swift
@@ -5,8 +5,8 @@ class SwiftMarkTests: XCTestCase {
     let markdown = Markdown()
     
     func testImageRender() {
-        let md = "![http://i.stack.imgur.com/HSiIL.png](Deployment Target Dropdown in Xcode)"
-        let html = "<img src=\"http://i.stack.imgur.com/HSiIL.png\" alt=\"Deployment Target Dropdown in Xcode\""
+        let md = "![Deployment Target Dropdown in Xcode](http://i.stack.imgur.com/HSiIL.png)"
+        let html = "<img src=\"http://i.stack.imgur.com/HSiIL.png\" alt=\"Deployment Target Dropdown in Xcode\"/>"
         do {
             let renderedMd = try markdown.render(md)
             XCTAssertEqual(html, renderedMd)
