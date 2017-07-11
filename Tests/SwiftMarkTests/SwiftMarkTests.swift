4fcfc03fbb7f32f745f40e3c0aee036d57b543d8
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-07-11 09:23:53 -0500

    Created SwiftMarkTest.test(md html) and .testLinkRender() methods

--- Tests/SwiftMarkTests/SwiftMarkTests.swift
+++ Tests/SwiftMarkTests/SwiftMarkTests.swift
@@ -4,21 +4,31 @@ import XCTest
 class SwiftMarkTests: XCTestCase {
     let markdown = Markdown()
     
-    func testImageRender() {
-        let md = "![Deployment Target Dropdown in Xcode](http://i.stack.imgur.com/HSiIL.png)"
-        let html = "<img src=\"http://i.stack.imgur.com/HSiIL.png\" alt=\"Deployment Target Dropdown in Xcode\"/>"
+    func test(markdown md: String, isEqualTo html: String) {
         do {
             let renderedMd = try markdown.render(md)
             XCTAssertEqual(html, renderedMd)
         } catch {
             XCTFail(error.localizedDescription)
         }
-        
+    }
+    
+    func testImageRender() {
+        let md = "![Deployment Target Dropdown in Xcode](http://i.stack.imgur.com/HSiIL.png)"
+        let html = "<img src=\"http://i.stack.imgur.com/HSiIL.png\" alt=\"Deployment Target Dropdown in Xcode\"/>"
+        test(markdown: md, isEqualTo: html)
+    }
+    
+    func testLinkRender() {
+        let md = "[Stack Overflow](https://stackoverflow.com/)"
+        let html = "<a href=\"https://stackoverflow.com/\">Stack Overflow</a>"
+        test(markdown: md, isEqualTo: html)
     }
     
     static var allTests : [(String, (SwiftMarkTests) -> () throws -> Void)] {
         return [
-                ("TestImageRender", testImageRender)
+                ("TestImageRender", testImageRender),
+                ("TestLinkRender", testLinkRender)
         ]
     }
 }
