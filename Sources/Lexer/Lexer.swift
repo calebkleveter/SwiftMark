223c7f888d4dfd4e9e544d580371393ba64565d6
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-07-11 11:00:18 -0500

    Changed code regex capture from .* to [^\\`]*

--- Sources/Lexer/Lexer.swift
+++ Sources/Lexer/Lexer.swift
@@ -43,7 +43,7 @@ open class Lexer {
         ("\\>\\s?([^\\n\\>]+)", ["$1"], { return .blockQuote(try Lexer().tokenize($0[0]))}),
         ("(\\+|\\-|\\*)\\s?(.+)", ["$2"], { return .unOrderedList(try Lexer().tokenize($0[0].safetyHTMLEncoded()))}),
         ("\\d+\\.\\s([^\\n]+)", ["$1"], { return .orderedList(try Lexer().tokenize($0[0].safetyHTMLEncoded()))}),
-        ("\\`(.*)\\`", ["$1"], { return .code($0[0].safetyHTMLEncoded())}),
+        ("\\`([^\\`]*)\\`", ["$1"], { return .code($0[0].safetyHTMLEncoded())}),
         ("\n{2}", [], { _ in return .break }),
         ("([^\\s]+)", ["$1"], { return .text($0[0].safetyHTMLEncoded())})
         
