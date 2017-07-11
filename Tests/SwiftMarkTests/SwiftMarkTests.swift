ace2de75f4d02ae2b9e9a5cd2f5ac7bf13e0beb9
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-07-11 11:00:45 -0500

    Adde styled multi-line paragraph to testParagraphRender() method

--- Tests/SwiftMarkTests/SwiftMarkTests.swift
+++ Tests/SwiftMarkTests/SwiftMarkTests.swift
@@ -42,6 +42,15 @@ class SwiftMarkTests: XCTestCase {
         
         html = "<p>Xcode 7.0.1 and iOS 9.1 are incompatible. You will need to update your version of Xcode via the Mac app store.</p><br/><p>If your iOS version is lower then the Xcode version on the other hand, you can change the deployment target for a lower version of iOS by going to the General Settings and under Deployment set your Deployment Target:</p>"
         test(markdown: md, isEqualTo: html)
+        
+        md = """
+        Xcode `7.0.1` and iOS `9.1` are *incompatible*. You will need to **update your version of Xcode** via the Mac app store.
+        
+        If your iOS version is lower then the Xcode version on the other hand, you can change the deployment target for a lower version of iOS by going to the **General Settings** and under Deployment set your *Deployment Target*:
+        """
+        
+        html = "<p>Xcode <code>7.0.1</code> and iOS <code>9.1</code> are <em>incompatible</em>. You will need to <strong>update your version of Xcode</strong> via the Mac app store.</p><br/><p>If your iOS version is lower then the Xcode version on the other hand, you can change the deployment target for a lower version of iOS by going to the <strong>General Settings</strong> and under Deployment set your <em>Deployment Target</em>:</p>"
+        test(markdown: md, isEqualTo: html)
     }
     
     static var allTests : [(String, (SwiftMarkTests) -> () throws -> Void)] {
