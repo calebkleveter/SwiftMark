0de74ff4d8dbc6ddc6f8ea1abeaef99d6eac2df5
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-07-11 06:36:24 -0500

    Wrote image render test

--- Tests/SwiftMarkTests/SwiftMarkTests.swift
+++ Tests/SwiftMarkTests/SwiftMarkTests.swift
@@ -2,7 +2,23 @@ import XCTest
 @testable import SwiftMark
 
 class SwiftMarkTests: XCTestCase {
+    let markdown = Markdown()
+    
+    func testImageRender() {
+        let md = "![http://i.stack.imgur.com/HSiIL.png](Deployment Target Dropdown in Xcode)"
+        let html = "<img src=\"http://i.stack.imgur.com/HSiIL.png\" alt=\"Deployment Target Dropdown in Xcode\""
+        do {
+            let renderedMd = try markdown.render(md)
+            XCTAssertEqual(html, renderedMd)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+        
+    }
+    
     static var allTests : [(String, (SwiftMarkTests) -> () throws -> Void)] {
-        return []
+        return [
+                ("TestImageRender", testImageRender)
+        ]
     }
 }
