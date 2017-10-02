4a3ac48d41b15aa0b809a03204b65af2821e3b58
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-10-02 10:22:20 -0500

    Created Version2 target

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftMark",
     targets: [
+        Target(name: "Version2"),
         Target(name: "Lexer"),
         Target(name: "Parser", dependencies: ["Lexer"]),
         Target(name: "Renderer", dependencies: ["Parser"]),
