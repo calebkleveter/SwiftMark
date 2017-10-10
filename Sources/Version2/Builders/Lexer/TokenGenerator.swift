2f679de50dd98bcf37fdf89c9cff2c89bffb1c9d
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-10-10 16:52:54 -0500

    Rewrote TokenGenerator.tokenize method to take in an array of Strings and to throw

--- Sources/Version2/Builders/Lexer/TokenGenerator.swift
+++ Sources/Version2/Builders/Lexer/TokenGenerator.swift
@@ -36,5 +36,5 @@ public protocol TokenGenerator {
     var templates: [String] { get }
     
     /// Takes in a match for the given pattern(s) and creates a token from it.
-    func tokenize(_ string: String) -> Token
+    func tokenize(_ strings: [String], position: TokenPositions)throws -> Token
 }
