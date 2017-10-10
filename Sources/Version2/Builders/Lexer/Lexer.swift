5f901fbcc8105eef9b901933a7fdac0eaa9f4277
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-10-10 12:25:28 -0500

    Created Lexer protocol

---
+++ Sources/Version2/Builders/Lexer/Lexer.swift
@@ -0,0 +1,31 @@
+//The MIT License (MIT)
+//
+//Copyright (c) 2017 Caleb Kleveter
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+/// A Lexer that converts text into an array of `Tokens` based on a RegEx match.
+public protocol Lexer {
+    
+    /// Converts a String into an array of `Token`s.
+    ///
+    /// - Parameter string: The text to tokenize.
+    /// - Returns: An array of tokens that contain the for the resulting text.
+    func tokenize(_ string: String) -> [Token]
+}
