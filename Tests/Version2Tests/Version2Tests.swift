bdf6bcb38a810ef8e470484a6819c4423fd69dd6
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-10-17 15:27:40 -0500

    Created ATXHeading test method

--- Tests/Version2Tests/Version2Tests.swift
+++ Tests/Version2Tests/Version2Tests.swift
@@ -101,9 +101,130 @@ class SwiftMarkTests: XCTestCase {
         test(markdown: md, isEqualTo: html)
     }
     
+    func testATXHeading() {
+        let md = """
+        # foo
+        ## foo
+        ### foo
+        #### foo
+        ##### foo
+        ###### foo
+
+        ####### foo
+
+        #5 bolt
+
+        #hashtag
+
+        \\## foo
+        
+        # foo *bar* \\*baz\\*
+        
+        #                  foo\u{0020}\u{0020}\u{0020}\u{0020}\u{0020}\u{0020}\u{0020}\u{0020}\u{0020}\u{0020}\u{0020}\u{0020}\u{0020}\u{0020}
+        
+         ### foo
+          ## foo
+           # foo
+        
+            # foo
+        
+        foo
+            # bar
+        
+        ## foo ##
+          ###   bar    ###
+        
+        # foo ##################################
+        ##### foo ##
+        
+        ### foo ###\u{0020}\u{0020}\u{0020}\u{0020}\u{0020}
+        
+        ### foo ### b
+        
+        # foo#
+        
+        ### foo \\###
+        ## foo #\\##
+        # foo \\#
+        
+        ****
+        ## foo
+        ****
+        
+        Foo bar
+        # baz
+        Bar foo
+        
+        ##\u{0020}
+        #\u{0020}
+        ### ###
+        """
+        
+        let html = """
+        <h1>foo</h1>
+        <h2>foo</h2>
+        <h3>foo</h3>
+        <h4>foo</h4>
+        <h5>foo</h5>
+        <h6>foo</h6>
+
+        ####### foo
+
+        #5 bolt
+
+        #hashtag
+
+        \\## foo
+
+        <h1>foo *bar* \\*baz\\*</h1>
+        
+        <h1>foo</h1>
+        
+        <h3>foo</h3>
+        <h2>foo</h2>
+        <h1>foo</h1>
+        
+            # foo
+        
+        foo
+            # bar
+        
+        <h2>foo</h2>
+        <h3>bar</h3>
+        
+        <h1>foo</h1>
+        <h5>foo</h5>
+        
+        <h3>foo</h3>
+        
+        <h3>foo ### b</h3>
+        
+        <h1>foo#</h1>
+        
+        <h3>foo \\###</h3>
+        <h2>foo \\###</h2>
+        <h1>foo \\#</h1>
+        
+        <hr />
+        <h2>foo</h2>
+        <hr />
+        
+        Foo bar
+        <h1>baz</h1>
+        Bar foo
+        
+        <h2></h2>
+        <h1></h1>
+        <h3></h3>
+        """
+        
+        test(markdown: md, isEqualTo: html)
+    }
+    
     static var allTests : [(String, (SwiftMarkTests) -> () throws -> Void)] {
         return [
-            ("testThematicBreak", testThematicBreak)
+            ("testThematicBreak", testThematicBreak),
+            ("testATXHeading", testATXHeading)
         ]
     }
 }
