7f7ef5cbe4ff96e4a941c26290c3f7fcda0ecc03
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-23 17:02:38 -0500

    Replaced Lexer.TokenGenerator type with TokenGenerator protocol

--- Sources/Lexer/Token.swift
+++ Sources/Lexer/Token.swift
@@ -1,18 +1,10 @@
 import Utilities
 
-extension Lexer {
-    public struct TokenGenerator {
-        public let generator: (inout CollectionTracker<String>) -> Lexer.Token?
-        
-        public init(generator: @escaping (inout CollectionTracker<String>) -> Lexer.Token?) {
-            self.generator = generator
-        }
-        
-        public func run(on tracker: inout CollectionTracker<String>) -> Lexer.Token? {
-            return self.generator(&tracker)
-        }
-    }
-    
+public protocol TokenGenerator {
+    func run(on tracker: inout CollectionTracker<String>) -> Lexer.Token?
+}
+
+extension Lexer {    
     public struct Token {
         public let name: String
         public let data: String
