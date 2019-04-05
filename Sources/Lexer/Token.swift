c4a2498d9b146bba670ff195932fe72e3fd71075
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-04-05 12:04:08 -0500

    Redesigned Lexer.Token struct with Storage enumm

--- Sources/Lexer/Token.swift
+++ Sources/Lexer/Token.swift
@@ -1,17 +1,27 @@
 import Utilities
 
 public protocol TokenGenerator {
-    func run(on tracker: inout CollectionTracker<String>) -> Lexer.Token?
+    func run(on tracker: inout CollectionTracker<[Lexer.Line]>) -> Lexer.Token?
 }
 
 extension Lexer {    
-    public struct Token {
+    public struct Token: Equatable {
+        public enum Storage: Equatable {
+            case raw(String)
+            case character
+        }
+        
         public let name: String
-        public let data: String
+        public let data: Storage
         
         public init(name: String, data: String) {
             self.name = name
-            self.data = data
+            self.data = .raw(data)
+        }
+        
+        public init(name: String) {
+            self.name = name
+            self.data = .character
         }
     }
 }
