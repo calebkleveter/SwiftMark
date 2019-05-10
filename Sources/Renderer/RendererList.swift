41a7601f1154a2614672ed5a1b65f02f8b5a3475
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-10 12:35:42 -0500

    Renamed RendererList.generators property to .renderers

--- Sources/Renderer/RendererList.swift
+++ Sources/Renderer/RendererList.swift
@@ -1,25 +1,25 @@
 public struct RendererList {
-    public let generators: [TokenRenderer]
+    public let renderers: [TokenRenderer]
     public let `default`: TokenRenderer
 
-    public init(generators: [TokenRenderer], `default`: TokenRenderer) {
-        self.generators = generators
+    public init(renderers: [TokenRenderer], `default`: TokenRenderer) {
+        self.renderers = renderers
         self.default = `default`
     }
 
-    public init(_ `default`: TokenRenderer, _ generators: TokenRenderer...) {
-        self.generators = generators
+    public init(_ `default`: TokenRenderer, _ renderers: TokenRenderer...) {
+        self.renderers = renderers
         self.default = `default`
     }
 }
 
-extension GeneratorList: ExpressibleByArrayLiteral {
+extension RendererList: ExpressibleByArrayLiteral {
     public init(arrayLiteral elements: TokenRenderer...) {
         guard elements.count > 0, let last = elements.last else {
             preconditionFailure("`GeneratorList` array literal must contain 1 or more elements")
         }
 
-        self = GeneratorList(generators: Array(elements[..<elements.index(before: elements.endIndex)]), default: last)
+        self = RendererList(renderers: Array(elements[..<elements.index(before: elements.endIndex)]), default: last)
     }
 }
 
