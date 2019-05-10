017f9a3742d246501e73ea333d2c48b3a8c84387
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-10 17:34:34 -0500

    Created ThematicBreakTests.testUnderscore and .testAsterisk test cases

--- Tests/SwiftMarkTests/RendererTests/ThematicBreakTests.swift
+++ Tests/SwiftMarkTests/RendererTests/ThematicBreakTests.swift
@@ -17,4 +17,32 @@ final class ThematicBreakTests: XCTestCase {
 
         XCTAssertEqual(html, "<hr/><hr/><hr/><hr/>")
     }
+
+    func testUnderscore() throws {
+        let text = """
+        _________________
+        ___
+           ___
+        __  _____        ________  __ _
+        """
+
+        let bytes = try self.renderer.render(markdown: Array(text.utf8))
+        let html = String(decoding: bytes, as: UTF8.self)
+
+        XCTAssertEqual(html, "<hr/><hr/><hr/><hr/>")
+    }
+
+    func testAsterisk() throws {
+        let text = """
+        ******************
+        ***
+           *****
+        **      *******     *** ** *      *   * *** * ***** ***
+        """
+
+        let bytes = try self.renderer.render(markdown: Array(text.utf8))
+        let html = String(decoding: bytes, as: UTF8.self)
+
+        XCTAssertEqual(html, "<hr/><hr/><hr/><hr/>")
+    }
 }
