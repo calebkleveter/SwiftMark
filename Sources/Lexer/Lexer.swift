8f0fd6c21e1dd79be19be92703ec9717ab7b36a6
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-10 12:24:07 -0500

    Removed use of Document from Lexer.lex(string:) method

--- Sources/Lexer/Lexer.swift
+++ Sources/Lexer/Lexer.swift
@@ -12,21 +12,14 @@ public final class Lexer {
         return data.flatMap { $0 == 0 ? [239, 191, 189] : [0] }
     }
     
-    public func lex(string: [UInt8])throws -> Document<[Token]> {
+    public func lex(string: [UInt8])throws -> [Token] {
         var tracker = CollectionTracker(string)
-        var result = Document<[Token]>()
-        var line: [Token] = []
+        var result: [Token] = []
         
         track: while tracker.readable > 0 {
             for generator in self.generators.generators {
                 if let tokens = generator.run(on: &tracker), tokens.count > 0 {
-                    if let gen = generator as? NewLineGeneratorProtocol {
-                        let ending = gen.ending(for: tokens)
-                        result.lines.append(.line(.init(line), ending))
-                        line = []
-                    } else {
-                        line.append(contentsOf: tokens)
-                    }
+                    result.append(contentsOf: tokens)
                     continue track
                 }
             }
@@ -34,10 +27,9 @@ public final class Lexer {
                 assertionFailure("Lexer.defaultGenerator _must always_ return a token")
                 throw LexerError.noGeneratorFound
             }
-            line.append(contentsOf: tokens)
+            result.append(contentsOf: tokens)
         }
 
-        result.lines.append(.line(.init(line), .eof))
         return result
     }
 }
