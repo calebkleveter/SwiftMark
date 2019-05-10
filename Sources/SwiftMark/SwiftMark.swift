5c947cfd5361af60414240faf14c8b8d56d12376
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-10 15:13:23 -0500

    Created SwiftMark class to render Markdown into HTML

--- Sources/SwiftMark/SwiftMark.swift
+++ Sources/SwiftMark/SwiftMark.swift
@@ -19,3 +19,42 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+
+import Lexer
+import Parser
+import Renderer
+import Utilities
+
+public struct Config {
+    public let renderers: (all: [Syntax], `default`: Syntax)
+
+    public init(renderers: [Syntax], `default`: Syntax) {
+        self.renderers = (renderers, `default`)
+    }
+}
+
+
+public final class SwiftMark {
+    public let config: Config
+
+    private let lexer: Lexer
+    private let parser: Parser
+    private let renderer: Renderer
+
+    public init(config: Config) {
+        self.config = config
+
+        self.lexer = Lexer(generators: .default)
+        self.parser = Parser(generators: .init(handlers: config.renderers.all, default: config.renderers.default))
+        self.renderer = Renderer(renderers: .init(handlers: config.renderers.all, default: config.renderers.default))
+    }
+
+    public func render(markdown: [UInt8])throws -> [UInt8] {
+        let cleaned = self.lexer.clean(data: markdown)
+        let lexed = try self.lexer.lex(string: cleaned)
+        let parsed = try self.parser.parse(tokens: lexed)
+        let rendered = try self.renderer.render(tokens: parsed)
+
+        return rendered
+    }
+}
