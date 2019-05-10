edf99eb09fdb6127c0629a81fabed2876fd2c46e
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-10 12:25:44 -0500

    Rebuilt Parser TokenGenerator and Parser.Token types to use nested data

--- Sources/Parser/Token.swift
+++ Sources/Parser/Token.swift
@@ -2,24 +2,46 @@ import Utilities
 import Lexer
 
 public protocol TokenGenerator {
-    func run(on tracker: Document<[Lexer.Token]>) -> Parser.Token?
+    func run(on tracker: inout CollectionTracker<[Lexer.Token]>) -> Parser.Token?
 }
 
 extension Parser {
     public struct Token {
         public let name: String
-        public let data: String
-        public let position: TokenPosition
-        
-        public init(name: String, data: String, position: TokenPosition) {
-            self.name = name
-            self.data = data
-            self.position = position
+
+        public var data: Data {
+            didSet {
+                switch self.data {
+                case .parserTokens, .raw: self.renderable = true
+                case .lexerTokens: self.renderable = false
+                }
+            }
         }
-    }
-    
-    public enum TokenPosition {
-        case open
-        case close
+
+        internal var renderable: Bool
+
+        public init(name: String, contents: [Lexer.Token]) {
+            self.name = name
+            self.data = .lexerTokens(contents)
+            self.renderable = false
+        }
+
+        public init(name: String, contents: [UInt8]) {
+            self.name = name
+            self.data = .raw(contents)
+            self.renderable = true
+        }
+
+        public init(name: String, data: [Parser.Token]) {
+            self.name = name
+            self.data = .parserTokens(data)
+            self.renderable = true
+        }
+
+        public enum Data {
+            case lexerTokens([Lexer.Token])
+            case parserTokens([Parser.Token])
+            case raw([UInt8])
+        }
     }
 }
