6fe12f73313c29788efafe527f857c471fd84d13
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-10 12:26:30 -0500

    Created first implementation of Parser.parse(tokens:) method

--- Sources/Parser/Parser.swift
+++ Sources/Parser/Parser.swift
@@ -1,1 +1,45 @@
-public final class Parser {}
+import Utilities
+import Lexer
+
+public final class Parser {
+    public let generators: GeneratorList
+
+    public init(generators: GeneratorList) {
+        self.generators = generators
+    }
+
+    public func parse(tokens: [Lexer.Token])throws -> [Parser.Token] {
+        var tracker = CollectionTracker(tokens)
+        var result: [Parser.Token] = []
+
+        track: while tracker.readable > 0 {
+            for generator in self.generators.generators {
+                if var token = generator.run(on: &tracker) {
+                    if token.renderable {
+                        result.append(token)
+                    } else {
+                        guard case let .lexerTokens(contents) = token.data else {
+                            assertionFailure("This case can technically never be reached. Please file a bug.")
+                            throw ParserError.noParsableTokens
+                        }
+
+                        let data = try self.parse(tokens: contents)
+                        token.data = .parserTokens(data)
+                        result.append(token)
+                    }
+
+                    continue track
+                }
+            }
+
+            guard let token = self.generators.default.run(on: &tracker) else {
+                assertionFailure("Parser.generators.default _must always_ return a token")
+                throw ParserError.noGeneratorFound
+            }
+
+            result.append(token)
+        }
+
+        return result
+    }
+}
