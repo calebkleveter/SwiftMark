4aff07a68a6ff70c58f81ef87acc0bf2680229d1
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-10 12:32:03 -0500

    Created RendererList type

---
+++ Sources/Renderer/RendererList.swift
@@ -0,0 +1,26 @@
+public struct GeneratorList {
+    public let generators: [TokenRenderer]
+    public let `default`: TokenRenderer
+
+    public init(generators: [TokenRenderer], `default`: TokenRenderer) {
+        self.generators = generators
+        self.default = `default`
+    }
+
+    public init(_ `default`: TokenRenderer, _ generators: TokenRenderer...) {
+        self.generators = generators
+        self.default = `default`
+    }
+}
+
+extension GeneratorList: ExpressibleByArrayLiteral {
+    public init(arrayLiteral elements: TokenRenderer...) {
+        guard elements.count > 0, let last = elements.last else {
+            preconditionFailure("`GeneratorList` array literal must contain 1 or more elements")
+        }
+
+        self = GeneratorList(generators: Array(elements[..<elements.index(before: elements.endIndex)]), default: last)
+    }
+}
+
+
