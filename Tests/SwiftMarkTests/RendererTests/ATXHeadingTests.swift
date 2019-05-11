05273a13d142929cbb9b4ed9d0d8408815485043
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-11 13:28:31 -0500

    Created ATXHeadingTests test suite with .testDepth test case

---
+++ Tests/SwiftMarkTests/RendererTests/ATXHeadingTests.swift
@@ -0,0 +1,23 @@
+import SwiftMark
+import XCTest
+
+final class ATXHeadingTests: XCTestCase {
+    let renderer = SwiftMark(config: Config(renderers: [], default: ATXHeading()))
+
+    func testDepth() throws {
+        let text = """
+        #
+        ##
+        ###
+        ####
+        #####
+        ######
+        """
+
+        let bytes = try self.renderer.render(markdown: Array(text.utf8))
+        let html = String(decoding: bytes, as: UTF8.self)
+
+        XCTAssertEqual(html, "<h1></h1><h2></h2><h3></h3><h4></h4><h5></h5><h6></h6>")
+    }
+}
+
