0b2774c40b4f41fe37e1af635b093c3a6d242e73
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-11 13:10:11 -0500

    Deleted LexerTests.testLines test case

--- Tests/LexerTests/LexerTests.swift
+++ Tests/LexerTests/LexerTests.swift
@@ -18,24 +18,6 @@ final class LexerTests: XCTestCase {
         return try! Array(Data(contentsOf: url))
     }()
     
-    func testLines() {
-        let rawLines = self.markdown.split(separator: 10, maxSplits: Int.max, omittingEmptySubsequences: false)
-        let lineCount = rawLines.count
-        let lines = lexer.lines(for: self.markdown)
-        
-//        XCTAssertEqual(lines.count, lineCount)
-        
-        let header: [UInt8] = [77, 97, 114, 107, 100, 111, 119, 110, 58, 32, 83, 121, 110, 116, 97, 120]
-        XCTAssertEqual(lines.first, Lexer.Line.line(CollectionTracker(header), .newLine))
-        XCTAssertEqual(lines.last, Lexer.Line.blank)
-        
-        measure {
-            for _ in 0...10_000 {
-                _ = lexer.lines(for: self.markdown)
-            }
-        }
-    }
-    
     func testMeasureLex() throws {
 
         // Baseline: 0.044
