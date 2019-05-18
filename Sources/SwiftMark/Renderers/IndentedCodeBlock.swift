6fc4551113d3639e7f285da0fee473fcb168be1c
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-18 14:39:48 -0500

    Properly handle all whitespace lines in IndentedCodeBlock parser

--- Sources/SwiftMark/Renderers/IndentedCodeBlock.swift
+++ Sources/SwiftMark/Renderers/IndentedCodeBlock.swift
@@ -17,11 +17,22 @@ public final class IndentedCodeBlock: Syntax {
         }
 
         var lines: [[UInt8]] = []
-        while tokens.peek(next: 4).map({ $0.name }) == Array(repeating: .newLine, count: 4) {
-            tokens.pop(next: 4)
+        while tokens.readable > 0 {
+            if tokens.peek(next: 4).filter({ $0.data == .raw([32]) }).count == 4 {
+                tokens.pop(next: 4)
+            } else {
+                let whitespace = tokens.whiteSpaceLine()
+                if whitespace.allWhiteSpace {
+                    lines.append(Array(repeating: 32, count: whitespace.length))
+                    tokens.pop(next: whitespace.length + 1)
+                    continue
+                } else {
+                    break
+                }
+            }
 
             let line = tokens.read(while: { $0.name != .newLine })
-            if line.count == 0 { break }
+            tokens.pop()
 
             let data = line.flatMap { token -> [UInt8]  in
                 switch token.data {
