ed396f4fe963d65d06012fb1ce9829cdf6845693
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-18 13:10:46 -0500

    Created ATXHeadingTests.testSuffix test case

--- Tests/SwiftMarkTests/RendererTests/ATXHeadingTests.swift
+++ Tests/SwiftMarkTests/RendererTests/ATXHeadingTests.swift
@@ -19,5 +19,20 @@ final class ATXHeadingTests: XCTestCase {
 
         XCTAssertEqual(html, "<h1></h1><h2></h2><h3></h3><h4></h4><h5></h5><h6></h6>")
     }
+
+    func testSuffix() throws {
+        let text = """
+        # ###
+        # #
+        ### #
+        ## ##
+        #### ######
+        """
+
+        let bytes = try self.renderer.render(markdown: Array(text.utf8))
+        let html = String(decoding: bytes, as: UTF8.self)
+
+        XCTAssertEqual(html, "<h1></h1><h1></h1><h3></h3><h2></h2><h4></h4>")
+    }
 }
 
