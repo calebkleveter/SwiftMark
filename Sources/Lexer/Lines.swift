1945d54931e32b90ad6ff81aaa29a075fb24671d
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-18 14:38:54 -0500

    Created CollectionTracker<[Lexer.Token]>.whiteSpaceLine() method

--- Sources/Lexer/Lines.swift
+++ Sources/Lexer/Lines.swift
@@ -5,4 +5,15 @@ extension CollectionTracker where Base == Array<Lexer.Token> {
         let last = self.peek(back: 1)
         return last.first?.name == .newLine || last == []
     }
+
+    public func whiteSpaceLine() -> (allWhiteSpace: Bool, length: Int) {
+        var length = 0
+
+        while self.peek(next: length + 1).last?.data == .raw([32]) {
+            length += 1
+        }
+
+        let next = self.peek(next: length + 1)
+        return (next.count == length || next.last?.name == .newLine, length)
+    }
 }
