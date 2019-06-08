c5e1ded21c93ea48038f6c5e2d834b045e0c19a4
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-06-08 12:53:04 -0500

    Created LinkReferenceTests.testRelativeLink test case

--- Tests/SwiftMarkTests/RendererTests/LinkReferenceTests.swift
+++ Tests/SwiftMarkTests/RendererTests/LinkReferenceTests.swift
@@ -7,7 +7,7 @@ final class LinkReferenceTests: XCTestCase {
     let parser = Parser(generators: [LinkReference()])
     let lexer = Lexer(generators: .default)
 
-    func testParsing() throws {
+    func testFullLink() throws {
         let text = """
         [Vapor]: <https://vapor.codes/> 'Vapor Website'
         """
@@ -22,5 +22,22 @@ final class LinkReferenceTests: XCTestCase {
 
         XCTAssertEqual(String(decoding: link.destination, as: UTF8.self), "https://vapor.codes/")
         XCTAssertEqual(link.title.map { String(decoding: $0, as: UTF8.self)}, "Vapor Website")
+    }
+
+    func testRelativeLink() throws {
+        let text = """
+        [Bio]: /about-me
+        """
+
+        let tokens = try self.lexer.lex(string: Array(text.utf8))
+        let ast = try self.parser.parse(tokens: tokens)
+
+        guard let link = ast.metadata["link-Bio"] as? Link else {
+            XCTFail("Expected `link-Bio` metadata to be a `Link` instance")
+            return
+        }
+
+        XCTAssertEqual(String(decoding: link.destination, as: UTF8.self), "/about-me")
+        XCTAssertEqual(link.title, nil)
     }
 }
